a0617e026dc46135c5526f55b8a7fa7b02d5c810
DOUGLAS KLEM PORTUGAL DO AMARAL <douglas.portugal@educacao.senac.rio>
2025-10-27 20:05:59 -0300

    primeira adição de arquivos e pastas

---
+++ UC1/Aula01/aula01.py
@@ -0,0 +1,31 @@
+# AULA 01 - Variáveis em Python (22/10/2025)
+
+# ## Categorias:
+
+# idade=30 #int
+# nome='Maria' #string
+# preco=19.99 #float
+# estoque=False #boolean
+# estoque_disponivel = None #nulo
+
+# print(idade)
+# print(type(idade))
+
+# cadastro=float(input('Digite um número: '))
+# print(cadastro)
+# print(type(cadastro))
+
+numero1=float(input('Diga seu 1o. número:'))
+numero2=float(input('Diga seu 2o. número:'))
+
+soma=numero1+numero2
+subtracao=numero1-numero2
+multi=numero1*numero2
+div=numero1/numero2
+resto=numero1%numero2
+
+print(f'Soma: {soma}')
+print(f'Subtração: {subtracao}')
+
+
+
---
+++ UC1/Aula02/aula02.py
@@ -0,0 +1,7 @@
+moto=True
+preco=20
+if preco > 10 or preco < 20:
+    print('Ok, não tá ruim assim...') 
+else:
+    print('Hoje não deu, prof! :(')
+
