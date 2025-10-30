aaef85a4cbbc2fe6a9daefe7124443d6be27f367
DOUGLAS KLEM PORTUGAL DO AMARAL <douglas.portugal@educacao.senac.rio>
2025-10-29 21:47:38 -0300

    script da aula 04

---
+++ UC1/Aula04/aula04.py
@@ -0,0 +1,43 @@
+# diadasemana=int(input('Qual dia da semana você quer saber?'))
+
+# if diadasemana==1:
+#     print('Domingo')
+# elif diadasemana==2:
+#     print('Segunda')
+# elif diadasemana==3:
+#     print('Terça')
+# elif diadasemana==4:
+#     print('Quarta')
+# elif diadasemana==5:
+#     print('Quinta')
+# elif diadasemana==6:
+#     print('Sexta')
+# elif diadasemana==7:
+#     print('Sábado')
+# else:
+#     print('Número inválido para essa pergunta.')
+try:
+    diadasemana=int(input('Qual dia da semana você quer saber?'))
+    match diadasemana:
+        case 1:
+            print('Domingo')
+        case 2:
+            print('Segunda')
+        case 3:
+            print('Terça')
+        case 4:
+            print('Quarta')
+        case 5:
+            print('Quinta')
+        case 6:
+            print('Sexta')
+        case 7:
+            print('Sábado')
+        case _:
+            print('Número de dia inválido. Informe um número entre 1 e 7.')
+except ValueError:
+    print('Informação inválida. Por favor informe um número inteiro.')
+
+
+
+
